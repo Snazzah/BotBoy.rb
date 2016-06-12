2487cf92eef3abc5a401c6d371d73cf5c20fca57
Snazzah <snazzypine25@yahoo.com>
2016-06-12 12:54:18 -1000

    Delete collectdata.php

--- collectdata.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_POST["token"]) && !empty($_POST["token"])){
-	if($_POST["token"] == "deck"){
-		if(isset($_POST["data"])){
-			file_put_contents("data.json", $_POST["data"]);
-		}
-	}
-}
